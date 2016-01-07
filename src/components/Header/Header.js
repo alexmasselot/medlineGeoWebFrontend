0e62aad9d642155a8b63afa1eb1c912cc42e5933
Alexandre Masselot <amasselot@octo.com>
2016-01-07 17:03:12 +0100

    images & nodes version

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -13,13 +13,12 @@ class Header extends Component {
     return (
       <div className="Header row">
         <div className="Header-container">
-          <a className="Header-brand" href="/" onClick={Link.handleClick}>
-            <img className="Header-brandImg" src={require('./logo-small.png')} width="38" height="38" alt="React" />
+          <a className="Header-brand" href="http://www.octo.ch">
+           <img className="Header-brandImg" src="/images/swiss-cow-octo.png" height="38" alt="octo" />
             <span className="Header-brandTxt">Medline geo</span>
           </a>
           <Navigation className="Header-nav" />
-          <div className="Header-banner">
-          </div>
+
         </div>
       </div>
     );
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -16,11 +16,11 @@ class Navigation extends Component {
   render() {
     return (
       <div className={classNames(this.props.className, 'Navigation')} role="navigation">
-        <a className="Navigation-link" href="/all" onClick={Link.handleClick}>all</a>
+        <a className="Navigation-link" href="/all" onClick={Link.handleClick}>all views</a>
         <a className="Navigation-link" href="/hexamap" onClick={Link.handleClick}>per location</a>
         <a className="Navigation-link" href="/country" onClick={Link.handleClick}>per country</a>
         <a className="Navigation-link" href="/country-pairs" onClick={Link.handleClick}>per country collaboration</a>
-        <span className="Navigation-spacer"> | </span>
+        <span className="Navigation-spacer"> | <a className="Navigation-link" href="http://blog.octo.com/QQQ">help</a></span>
       </div>
     );
   }
